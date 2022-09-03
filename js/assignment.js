7ba5f6b392e4c8d842ca5282bffd5497f7fd0d9b
YasinSajeeb <yeasinalhossen@gmail.com>
2022-09-03 14:54:01 +0600

    fourth commit

--- js/assignment.js
+++ js/assignment.js
@@ -1,1 +1,23 @@
-console.log("let's start");
+const url = 'https://openapi.programming-hero.com/api/news/categories';
+
+const loadCategories = () => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCategories(data.data.news_category));
+}
+
+const displayCategories = categories =>{
+    // console.log(categories);
+    const ul = document.getElementById('news-categories')
+    categories.forEach(category =>{
+        console.log(category);
+        const li = document.createElement('li');
+        li.classList.add('nav-item');
+        li.innerHTML = `
+        <a class="nav-link" aria-current="page" href="#">${category.category_name}</a>
+        `;
+        ul.appendChild(li);
+    })
+}
+    
+loadCategories();
